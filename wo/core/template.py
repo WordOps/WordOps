72ed4d820abae9cf0632874c714818a866360440
VirtuBox <thomas@virtubox.net>
2019-08-19 18:23:29 +0200

    Create .orig version of custom config

--- wo/core/template.py
+++ wo/core/template.py
@@ -8,9 +8,9 @@ Render Templates
 
 class WOTemplate():
     def tmpl_render(self, fileconf, template, data, overwrite=True):
+        data = dict(data)
         if (not overwrite):
             if not os.path.isfile('{0}'.format(fileconf)):
-                data = dict(data)
                 Log.debug(self, 'Writting the configuration to '
                           'file {0}'.format(fileconf))
                 wo_template = open('{0}'.format(fileconf),
@@ -21,10 +21,17 @@ class WOTemplate():
         else:
             if (not os.path.isfile('{0}.custom'
                                    .format(fileconf))):
-                data = dict(data)
                 Log.debug(self, 'Writting the configuration to '
                           'file {0}'.format(fileconf))
                 wo_template = open('{0}'.format(fileconf),
+                                   encoding='utf-8', mode='w')
+                self.app.render((data), '{0}'.format(template),
+                                out=wo_template)
+                wo_template.close()
+            else:
+                Log.debug(self, 'Writting the configuration to '
+                          'file {0}.orig'.format(fileconf))
+                wo_template = open('{0}.orig'.format(fileconf),
                                    encoding='utf-8', mode='w')
                 self.app.render((data), '{0}'.format(template),
                                 out=wo_template)
