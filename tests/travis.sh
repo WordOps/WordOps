8162fa550f3a5341b1a6d5c83bbae7db3aaa84ea
VirtuBox <thomas@virtubox.net>
2019-08-30 01:02:24 +0200

    Update travis

--- tests/travis.sh
+++ tests/travis.sh
@@ -46,7 +46,8 @@ if ! {
     echo -e '       wo site update              '
     echo -e "${CGREEN}#############################################${CEND}"
     wo site create 1.com --html && wo site create 2.com --php && wo site create 3.com --mysql
-    wo site update 1.com --wp && wo site update 2.com --php73 && wo site update 3.com --php73 && wo site update 1.com --wpfc && wo site update 1.com --wpsc && wo site update 1.com --wpredis
+    wo site update 1.com --wp && wo site update 2.com --php73 && wo site update 3.com --php73
+    wo site update 1.com --wp && wo site update 1.com --wpfc && wo site update 1.com --wpsc && wo site update 1.com --wpredis && wo site update 1.com --wpce && wo site update 1.com --wprocket && wo site update 1.com --php73=off
 }; then
     exit_script
 fi
