93ef756cb14c57c3adda2d818005f0a85db98ac3
VirtuBox <thomas@virtubox.net>
2019-09-05 12:22:31 +0200

    Adjust length

--- wo/core/logging.py
+++ wo/core/logging.py
@@ -50,8 +50,8 @@ class Log:
         """
         Logs info messages with validation step
         """
-        space_to_add = int(30 - len(msg))
-        space = "                    "
+        space_to_add = int(40 - len(msg[0:40]))
+        space = "                             "
         print(
             Log.OKBLUE + "{0}".format(msg[0:30]) +
             "{0}".format(space[0:space_to_add]) +
@@ -63,10 +63,10 @@ class Log:
         """
         Logs info messages after validation step
         """
-        space_to_add = int(30 - len(msg))
-        space = "                    "
+        space_to_add = int(40 - len(msg[0:40]))
+        space = "                              "
         print(
-            Log.OKBLUE + "{0}".format(msg[0:30]) +
+            Log.OKBLUE + "{0}".format(msg[0:40]) +
             "{0}".format(space[0:space_to_add]) +
             " [" + Log.ENDC + Log.OKGREEN + "OK" +
             Log.ENDC + Log.OKBLUE + "]" + Log.ENDC, end=end)
@@ -77,10 +77,10 @@ class Log:
         """
         Logs info messages after validation step
         """
-        space_to_add = int(30 - len(msg))
-        space = "                    "
+        space_to_add = int(40 - len(msg[0:40]))
+        space = "                             "
         print(
-            Log.OKBLUE + "{0}".format(msg[0:30]) +
+            Log.OKBLUE + "{0}".format(msg[0:40]) +
             "{0}".format(space[0:space_to_add]) +
             " [" + Log.ENDC + Log.FAIL + "OK" +
             Log.ENDC + Log.OKBLUE + "]" + Log.ENDC, end=end)
