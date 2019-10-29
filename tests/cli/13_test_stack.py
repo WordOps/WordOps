290e9edeb9c4851361e9b947c05d81b9ac515e10
VirtuBox <thomas@virtubox.net>
2019-10-29 15:22:22 +0100

    Fix tests

--- tests/cli/13_test_stack.py
+++ tests/cli/13_test_stack.py
@@ -8,48 +8,40 @@ class CliTestCaseStack(test.WOTestCase):
         self.app.setup()
         self.app.run()
 
-
     def test_wo_cli_stack_install_nginx(self):
         self.app = get_test_app(argv=['stack', 'install', '--nginx'])
         self.app.setup()
         self.app.run()
-
 
     def test_wo_cli_stack_install_php(self):
         self.app = get_test_app(argv=['stack', 'install', '--php'])
         self.app.setup()
         self.app.run()
 
-
     def test_wo_cli_stack_install_php73(self):
         self.app = get_test_app(argv=['stack', 'install', '--php73'])
         self.app.setup()
         self.app.run()
-
 
     def test_wo_cli_stack_install_mysql(self):
         self.app = get_test_app(argv=['stack', 'install', '--mysql'])
         self.app.setup()
         self.app.run()
 
-
     def test_wo_cli_stack_install_wpcli(self):
         self.app = get_test_app(argv=['stack', 'install', '--wpcli'])
         self.app.setup()
         self.app.run()
-
 
     def test_wo_cli_stack_install_phpmyadmin(self):
         self.app = get_test_app(argv=['stack', 'install', '--phpmyadmin'])
         self.app.setup()
         self.app.run()
 
-
     def test_wo_cli_stack_install_adminer(self):
         self.app = get_test_app(argv=['stack', 'install', '--adminer'])
         self.app.setup()
         self.app.run()
-
 
     def test_wo_cli_stack_install_utils(self):
         self.app = get_test_app(argv=['stack', 'install', '--utils'])
