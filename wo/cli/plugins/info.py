4652ea6ceb6d8a10c48e71f78f8af556b9ddb729
VirtuBox <thomas@virtubox.net>
2019-10-30 05:30:13 +0100

    Fix info

--- wo/cli/plugins/info.py
+++ wo/cli/plugins/info.py
@@ -79,22 +79,20 @@ class WOInfoController(CementBaseController):
         max_execution_time = config['PHP']['max_execution_time']
 
         config.read('/etc/{0}/fpm/pool.d/www.conf'.format("php/7.2"))
-        wo_sec = config.sections()
-        config.read('/etc/{0}/fpm/pool.d/www.conf'.format("php/7.2"))
-        www_listen = config[wo_sec[0]]['listen']
-        www_ping_path = config[wo_sec[0]]['ping.path']
-        www_pm_status_path = config[wo_sec[0]]['pm.status_path']
-        www_pm = config[wo_sec[0]]['pm']
-        www_pm_max_requests = config[wo_sec[0]]['pm.max_requests']
-        www_pm_max_children = config[wo_sec[0]]['pm.max_children']
-        www_pm_start_servers = config[wo_sec[0]]['pm.start_servers']
-        www_pm_min_spare_servers = config[wo_sec[0]]['pm.min_spare_servers']
-        www_pm_max_spare_servers = config[wo_sec[0]]['pm.max_spare_servers']
-        www_request_terminate_time = (config[wo_sec[0]]
+        www_listen = config['www-php72']['listen']
+        www_ping_path = config['www-php72']['ping.path']
+        www_pm_status_path = config['www-php72']['pm.status_path']
+        www_pm = config['www-php72']['pm']
+        www_pm_max_requests = config['www-php72']['pm.max_requests']
+        www_pm_max_children = config['www-php72']['pm.max_children']
+        www_pm_start_servers = config['www-php72']['pm.start_servers']
+        www_pm_min_spare_servers = config['www-php72']['pm.min_spare_servers']
+        www_pm_max_spare_servers = config['www-php72']['pm.max_spare_servers']
+        www_request_terminate_time = (config['www-php72']
                                             ['request_terminate_timeout'])
         try:
             www_xdebug = (
-                config[wo_sec[0]]['php_admin_flag[xdebug.profiler_enable'
+                config['www-php72']['php_admin_flag[xdebug.profiler_enable'
                                   '_trigger]'])
         except Exception as e:
             Log.debug(self, "{0}".format(e))
@@ -159,21 +157,19 @@ class WOInfoController(CementBaseController):
         max_execution_time = config['PHP']['max_execution_time']
 
         config.read('/etc/php/7.3/fpm/pool.d/www.conf')
-        wo_sec = config.sections()
-        config.read('/etc/php/7.3/fpm/pool.d/www.conf')
-        www_listen = config[wo_sec[0]]['listen']
-        www_ping_path = config[wo_sec[0]]['ping.path']
-        www_pm_status_path = config[wo_sec[0]]['pm.status_path']
-        www_pm = config[wo_sec[0]]['pm']
-        www_pm_max_requests = config[wo_sec[0]]['pm.max_requests']
-        www_pm_max_children = config[wo_sec[0]]['pm.max_children']
-        www_pm_start_servers = config[wo_sec[0]]['pm.start_servers']
-        www_pm_min_spare_servers = config[wo_sec[0]]['pm.min_spare_servers']
-        www_pm_max_spare_servers = config[wo_sec[0]]['pm.max_spare_servers']
-        www_request_terminate_time = (config[wo_sec[0]]
+        www_listen = config['www-php73']['listen']
+        www_ping_path = config['www-php73']['ping.path']
+        www_pm_status_path = config['www-php73']['pm.status_path']
+        www_pm = config['www-php73']['pm']
+        www_pm_max_requests = config['www-php73']['pm.max_requests']
+        www_pm_max_children = config['www-php73']['pm.max_children']
+        www_pm_start_servers = config['www-php73']['pm.start_servers']
+        www_pm_min_spare_servers = config['www-php73']['pm.min_spare_servers']
+        www_pm_max_spare_servers = config['www-php73']['pm.max_spare_servers']
+        www_request_terminate_time = (config['www-php73']
                                             ['request_terminate_timeout'])
         try:
-            www_xdebug = (config[wo_sec[0]]
+            www_xdebug = (config['www-php73']
                           ['php_admin_flag[xdebug.profiler_enable'
                            '_trigger]'])
         except Exception as e:
