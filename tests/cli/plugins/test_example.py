d14369cc7697ab4a12ab95c7563c2ed08a9783f0
VirtuBox <thomas@virtubox.net>
2019-11-11 13:23:10 +0100

    Fix test example

--- tests/cli/plugins/test_example.py
+++ tests/cli/plugins/test_example.py
@@ -1,9 +1,10 @@
 """Tests for Example Plugin."""
 
 from wo.utils import test
+from wo.cli.main import WOTestApp
 
 
 class ExamplePluginTestCase(test.WOTestCase):
     def test_load_example_plugin(self):
-        self.app.setup()
-        self.app.plugin.load_plugin('example')
+        with WOTestApp as app:
+            app.plugin.load_plugin('example')
