81fbb72fe176313427f87f21f85b74c9bb48e539
VirtuBox <thomas@virtubox.net>
2020-01-17 11:57:15 +0100

    Fix webroot variable in template

--- wo/cli/plugins/secure.py
+++ wo/cli/plugins/secure.py
@@ -117,7 +117,8 @@ class WOSecureController(CementBaseController):
                 Log.info(self, "Please Enter valid port number :")
                 port = input("WordOps admin port [22222]:")
             pargs.user_input = port
-        data = dict(release=WOVar.wo_version, port=pargs.user_input)
+        data = dict(release=WOVar.wo_version,
+                    port=pargs.user_input, webroot='/var/www/')
         WOTemplate.deploy(
             self, '/etc/nginx/sites-available/22222',
             '22222.mustache', data)
