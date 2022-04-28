4cac21edf1b70114e7747afa63feaa1732080879
VirtuBox <thomas@virtubox.net>
2022-04-28 12:50:41 +0200

    Last fix for git safe directory

--- wo/core/git.py
+++ wo/core/git.py
@@ -29,7 +29,8 @@ class WOGit:
                         Log.debug(self, "{0}".format(e))
                         Log.error(self, "Unable to git init at {0}"
                                   .format(path))
-                git.config("--global --add safe.directory", "{0}".format(path))
+                git.config("--global", "--add", "safe.directory",
+                           "{0}".format(path))
                 status = wogit.status("-s")
                 if len(status.splitlines()) > 0:
                     try:
